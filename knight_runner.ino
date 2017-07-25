90e9d773db763cacfc969c4a5296f8e67daca01f
kdragon1432 <kellydong1432@gmail.com>
2017-07-25 11:07:27 -0400

    Add files via upload

---
+++ knight_runner.ino
@@ -0,0 +1,20 @@
+int del=100; 
+void setup() {
+ 
+  for (int i = 1; i<=14 ; i += 2) {
+    pinMode(i, OUTPUT);
+  } 
+} 
+ 
+void loop() {
+  for (int i = 1; i<=14; i += 2) { 
+    digitalWrite(i, HIGH);
+    delay(del);
+    digitalWrite(i, LOW);
+  }
+  for (int i = 13; i>=0; i -= 2) { 
+    digitalWrite(i, HIGH);
+    delay(del);
+    digitalWrite(i, LOW);
+  }
+}
